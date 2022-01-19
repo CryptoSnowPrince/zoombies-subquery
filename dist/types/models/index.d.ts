d69993e8a33efabd0610752ac566ffd911585d91
DESKTOP-3J65I2E\admin <RogerDTilton@gmail.com>
2022-01-19 09:16:37 +0900

    remove dist and src/types

--- dist/types/models/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { LogCardTypeLoaded } from "./LogCardTypeLoaded";
-export { LogCardMinted } from "./LogCardMinted";
-export { LogPackOpened } from "./LogPackOpened";
-export { LogSponsorLinked } from "./LogSponsorLinked";
-export { LogSponsorReward } from "./LogSponsorReward";
-export { LogDailyReward } from "./LogDailyReward";
-export { LogRewardBoosters } from "./LogRewardBoosters";
-export { LogSacrificeNFT } from "./LogSacrificeNFT";
